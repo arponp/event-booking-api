8e6a5fae59a530ad2d5984bcc12e1a6a0f35ff0d
Arpon Purkayastha <arponp30@gmail.com>
2022-05-24 23:17:24 -0700

    add basic RootQuery and RootMutation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
 import bodyParser from "body-parser";
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery 
+            mutation: RootMutation 
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Cooking", "Sailing", "Coding"];
+      },
+      createEvent: (args: { name: String }) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(3000, () => console.log("Server Live"));
